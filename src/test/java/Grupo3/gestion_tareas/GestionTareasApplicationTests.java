775abf6349b939d3bc2dde0e66ee306cf16a2a29
Jesus <U21309992@utp.edu.pe>
2026-04-21 20:21:20 -0500

    Inicializa proyecto Spring Boot

---
+++ src/test/java/Grupo3/gestion_tareas/GestionTareasApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo3.gestion_tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
