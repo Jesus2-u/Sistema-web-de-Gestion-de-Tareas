c4d297489a148980219e558b20d0bb3c9a6e078f
Darwin <rj45xy@gmail.com>
2026-04-24 23:19:40 -0500

    Creación del CRUD Asignacion

---
+++ src/main/java/Grupo3/gestion_tareas/controller/AsignacionController.java
@@ -0,0 +1,41 @@
+package Grupo3.gestion_tareas.controller;
+import Grupo3.gestion_tareas.model.Asignacion;
+import Grupo3.gestion_tareas.repository.AsignacionRepository;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+@RestController
+@RequestMapping("/asignacion")
+@CrossOrigin
+public class AsignacionController {
+    private final AsignacionRepository asignacionRepository;
+    public AsignacionController(AsignacionRepository asignacionRepository) {
+        this.asignacionRepository = asignacionRepository;
+    }
+    // GET listar
+    @GetMapping
+    public List<Asignacion> listar() {
+        return asignacionRepository.findAll();
+    }
+    // POST crear
+    @PostMapping
+    public Asignacion crear(@RequestBody Asignacion asignacion) {
+        return asignacionRepository.save(asignacion);
+    }
+    // PUT actualizar
+    @PutMapping("/{id}")
+    public Asignacion actualizar(@PathVariable Long id, @RequestBody Asignacion nuevaAsignacion) {
+        Asignacion asignacion = asignacionRepository.findById(id).orElse(null);
+        if (asignacion == null) {
+            throw new RuntimeException("Asignación no encontrada");
+        }
+        asignacion.setUsuarioId(nuevaAsignacion.getUsuarioId());
+        asignacion.setTareaId(nuevaAsignacion.getTareaId());
+        asignacion.setFechaAsignacion(nuevaAsignacion.getFechaAsignacion());
+        return asignacionRepository.save(asignacion);
+    }
+    // DELETE
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        asignacionRepository.deleteById(id);
+    }
+}
